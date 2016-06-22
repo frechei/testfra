053bf3b47037c448f7f8a6d4da3cebf2aafd4ef6
haitao <758277560@qq.com>
2016-06-22 11:54:09 +0800

    cup armv8 检测

--- src/org/haitao/common/utils/AppUtils.java
+++ src/org/haitao/common/utils/AppUtils.java
@@ -151,77 +151,82 @@ public class AppUtils
 	 * @return 
 	 * 三个参数类型的数组，第一个参数标识是不是ARM架构，第二个参数标识是V6还是V7架构，第三个参数标识是不是neon指令集
 	 */
-	public static Object[] getCpuArchitecture() {
-		Object mArmArchitecture[] = new Object[3];
-/*		if ((Integer) mArmArchitecture[1] != -1) {
-			return mArmArchitecture;
-		}*/
-		try {
-			InputStream is = new FileInputStream("/proc/cpuinfo");
-			InputStreamReader ir = new InputStreamReader(is);
-			BufferedReader br = new BufferedReader(ir);
-			try {
-				String nameProcessor = "Processor";
-				String nameFeatures = "Features";
-				String nameModel = "model name";
-				String nameCpuFamily = "cpu family";
-				while (true) {
-					String line = br.readLine();
-					String[] pair = null;
-					if (line == null) {
-						break;
-					}
-					pair = line.split(":");
-					if (pair.length != 2)
-						continue;
-					String key = pair[0].trim();
-					String val = pair[1].trim();
-					if (key.compareTo(nameProcessor) == 0) {
-						String n = "";
-						for (int i = val.indexOf("ARMv") + 4; i < val.length(); i++) {
-							String temp = val.charAt(i) + "";
-							if (temp.matches("\\d")) {
-								n += temp;
-							} else {
-								break;
-							}
-						}
-						mArmArchitecture[0] = "ARM";
-						mArmArchitecture[1] = Integer.parseInt(n);
-						continue;
-					}
+	 public static Object[] getCpuArchitecture() {
+	        Object[] mArmArchitecture = new Object[3];
+	        try {
+	            InputStream is = new FileInputStream("/proc/cpuinfo");
+	            InputStreamReader ir = new InputStreamReader(is);
+	            BufferedReader br = new BufferedReader(ir);
+	            try {
+	                String nameProcessor = "Processor";
+	                String nameFeatures = "Features";
+	                String nameModel = "model name";
+	                String nameCpuFamily = "cpu family";
+	                while (true) {
+	                    String line = br.readLine();
+	                    String[] pair = null;
+	                    if (line == null) {
+	                        break;
+	                    }
+	                    pair = line.split(":");
+	                    if (pair.length != 2)
+	                        continue;
+	                    String key = pair[0].trim();
+	                    String val = pair[1].trim();
+	                    if (key.compareTo(nameProcessor) == 0) {
+	                        String n = "";
+	                        for (int i = val.indexOf("ARMv") + 4; i < val.length(); i++) {
+	                            String temp = val.charAt(i) + "";
+	                            if (temp.matches("\\d")) {
+	                                n += temp;
+	                            } else {
+	                                break;
+	                            }
+	                        }
+	                        mArmArchitecture[0] = "ARM";
+	                        if ("".equals(n)){
+	                            if (val.toLowerCase().contains("aarch64")){
+	                                mArmArchitecture[1] = "8";
+	                            }else{
+	                                mArmArchitecture[1] = "unknown";
+	                            }
+	                        }else{
+	                            mArmArchitecture[1] = Integer.parseInt(n);
+	                        }
+	                        continue;
+	                    }
 
-					if (key.compareToIgnoreCase(nameFeatures) == 0) {
-						if (val.contains("neon")) {
-							mArmArchitecture[2] = "neon";
-						}
-						continue;
-					}
+	                    if (key.compareToIgnoreCase(nameFeatures) == 0) {
+	                        if (val.contains("neon")) {
+	                            mArmArchitecture[2] = "neon";
+	                        }
+	                        continue;
+	                    }
 
-					if (key.compareToIgnoreCase(nameModel) == 0) {
-						if (val.contains("Intel")) {
-							mArmArchitecture[0] = "INTEL";
-							mArmArchitecture[2] = "atom";
-						}
-						continue;
-					}
+	                    if (key.compareToIgnoreCase(nameModel) == 0) {
+	                        if (val.contains("Intel")) {
+	                            mArmArchitecture[0] = "INTEL";
+	                            mArmArchitecture[2] = "atom";
+	                        }
+	                        continue;
+	                    }
 
-					if (key.compareToIgnoreCase(nameCpuFamily) == 0) {
-						mArmArchitecture[1] = Integer.parseInt(val);
-						continue;
-					}
-				}
-			} finally {
-				br.close();
-				ir.close();
-				is.close();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	                    if (key.compareToIgnoreCase(nameCpuFamily) == 0) {
+	                        mArmArchitecture[1] = Integer.parseInt(val);
+	                        continue;
+	                    }
+	                }
+	            } finally {
+	                br.close();
+	                ir.close();
+	                is.close();
+	            }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
 
-		return mArmArchitecture;
-	}
+	        return mArmArchitecture;
+	    }
     /**
      * 获取手机系统SDK版本
      * 
