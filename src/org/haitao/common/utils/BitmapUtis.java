7d88e5230a5b8a44e0753414875c6affccd6c3e6
haitao <758277560@qq.com>
2016-08-02 14:57:21 +0800

    磁盘缓存

--- src/org/haitao/common/utils/BitmapUtis.java
+++ src/org/haitao/common/utils/BitmapUtis.java
@@ -1,5 +1,6 @@
 package org.haitao.common.utils;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -12,7 +13,10 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PixelFormat;
 import android.graphics.PorterDuff.Mode;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
@@ -441,4 +445,60 @@ public class BitmapUtis {
 //    mFileTypes.put("1F8B08", "gz");
 //    mFileTypes.put("", "");
 //    mFileTypes.put("", "");
+    public static byte[] bitmap2Bytes(Bitmap bm)
+    {
+        if (bm == null)
+        {
+            return null;
+        }
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        return baos.toByteArray();
+    }
+
+    public static Bitmap bytes2Bitmap(byte[] bytes)
+    {
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+
+
+    /**
+     * Drawable → Bitmap
+     */
+    public static Bitmap drawable2Bitmap(Drawable drawable)
+    {
+        if (drawable == null)
+        {
+            return null;
+        }
+        // 取 drawable 的长宽
+        int w = drawable.getIntrinsicWidth();
+        int h = drawable.getIntrinsicHeight();
+        // 取 drawable 的颜色格式
+        Bitmap.Config config = drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565;
+        // 建立对应 bitmap
+        Bitmap bitmap = Bitmap.createBitmap(w, h, config);
+        // 建立对应 bitmap 的画布
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, w, h);
+        // 把 drawable 内容画到画布中
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    /*
+         * Bitmap → Drawable
+		 */
+    @SuppressWarnings("deprecation")
+    public static Drawable bitmap2Drawable(Bitmap bm)
+    {
+        if (bm == null)
+        {
+            return null;
+        }
+        BitmapDrawable bd = new BitmapDrawable(bm);
+        bd.setTargetDensity(bm.getDensity());
+        return new BitmapDrawable(bm);
+    }
+    
 }
